bc7485f9c78b72f9d29f00bfdcdcb10f9d10c7ce
Felismino Chico <felisminochico35@gmail.com>
2024-11-07 17:18:16 +0000

    Feito no GitHub

--- javascript/geral.js
+++ javascript/geral.js
@@ -7,11 +7,13 @@ function menu(){
         window.document.getElementById('celular-520px').style.left = '0px';
         window.document.getElementsByTagName('main')[0].style.filter = 'blur(1px)';
         window.document.getElementById('cabecalho').style.filter = 'blur(1px)'
+        window.document.getElementsByTagName('footer')[0].style.filter = 'blur(1px)'
         Menu = true;
     }else{
         window.document.getElementById('celular-520px').style.left = '-500px'
         window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
         window.document.getElementById('cabecalho').style.filter = 'blur(0px)'
+        window.document.getElementsByTagName('footer')[0].style.filter = 'blur(0px)'
         Menu = false;
     }
 }
@@ -20,5 +22,6 @@ function fecharMenu(){
     window.document.getElementById('celular-520px').style.left = '-500px'
     window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
     window.document.getElementById('cabecalho').style.filter = 'blur(0px)'
+    window.document.getElementsByTagName('footer')[0].style.filter = 'blur(0px)'
     Menu = false;
 }
