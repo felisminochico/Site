d30f0014804fccd26c92d1ca096b060d5fda70fe
Felismino Chico <felisminochico35@gmail.com>
2024-11-07 17:10:28 +0000

    Muitas atualizações.

--- javascript/geral.js
+++ javascript/geral.js
@@ -5,14 +5,20 @@ let Menu = false;
 function menu(){
     if(Menu == false){
         window.document.getElementById('celular-520px').style.left = '0px';
+        window.document.getElementsByTagName('main')[0].style.filter = 'blur(1px)';
+        window.document.getElementById('cabecalho').style.filter = 'blur(1px)'
         Menu = true;
     }else{
         window.document.getElementById('celular-520px').style.left = '-500px'
+        window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
+        window.document.getElementById('cabecalho').style.filter = 'blur(0px)'
         Menu = false;
     }
 }
 
 function fecharMenu(){
     window.document.getElementById('celular-520px').style.left = '-500px'
+    window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
+    window.document.getElementById('cabecalho').style.filter = 'blur(0px)'
     Menu = false;
 }
