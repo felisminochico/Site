5952e18ea94fe5bffe367014beb8d05677c7f4c1
Felismino Chico <felisminochico35@gmail.com>
2024-12-03 09:50:22 +0000

    Novas modificações

--- javascript/geral.js
+++ javascript/geral.js
@@ -1,10 +1,23 @@
 // alert('Olá, Mundo!');
 let figureFotoDesenvolvedor260px = window.document.getElementById('figura-foto-desenvolvedor-260px');
 let figcaptionLegendaDesenvolvedor260px = window.document.getElementById('legenda-desenvolvedor-260px');
+let conteudoHora = window.document.getElementById('conteudo-hora');
+let hora = new Date().getHours();
 let Menu = false;
 
 figureFotoDesenvolvedor260px.addEventListener('mouseover', moverFotoDesenvolvedor260px)
 figureFotoDesenvolvedor260px.addEventListener('mouseout', tirarmouseFotoDesenvolvedor260px)
+
+if(hora < 12){
+    conteudoHora.innerHTML = 'Muito Bom Dia, Caríssimo Visitante!';
+    conteudoHora.style.color = '#8C6330';
+}else if(hora < 18){
+    conteudoHora.innerHTML = 'Muito Boa Tarde, Caríssimo Visitante!';
+    conteudoHora.style.color = '#03588C';
+}else if(hora >= 18){
+    conteudoHora.innerHTML = 'Muito Boa Noite, Caríssimo Visitante!';
+    conteudoHora.style.color = '#023859';
+}
 
 function moverFotoDesenvolvedor260px(){
     figcaptionLegendaDesenvolvedor260px.style.opacity = 1;
